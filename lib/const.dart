4ad21f8169012ce6138ef8f06e007421a3bafb49
Andrés Escobar <andres.escobar.9103@gmail.com>
2021-05-27 13:30:10 -0400

    Fix bundle ID

--- lib/const.dart
+++ lib/const.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 
-final themeColor = Color(0xff2075e2);
+final themeColor = Color.fromRGBO(56, 167, 222, 1);
 final primaryColor = Color(0xff203152);
 final greyColor = Color(0xffaeaeae);
 final greyColor2 = Color(0xffE8E8E8);
